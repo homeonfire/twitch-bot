3cee64c4f068fbf728c47ea07e1c1fc78f70b2ac
Ivan <getintouch.hof@gmail.com>
2026-02-21 20:58:41 +0300

    Добавил Caddy и настройку SSL

--- routes/api.php
+++ routes/api.php
@@ -28,7 +28,8 @@ Route::get('/tts/{channel}/next', function ($channel) {
                 'Content-Type' => 'application/json'
             ])->post("https://api.elevenlabs.io/v1/text-to-speech/{$bot->elevenlabs_voice_id}", [
                 'text' => $message->message,
-                'model_id' => 'eleven_multilingual_v2'
+                'model_id' => 'eleven_turbo_v2_5',
+                'language_code' => 'ru',
             ]);
 
             if ($response->successful()) {
